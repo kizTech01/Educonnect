7c71cbcf7173819afbde1e5a34b5b9122154d4a0
KizTech <kiztech01@gmail.com>
2026-09-20 16:47:47 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.sitemaps.views import sitemap
+from portal.sitemaps import StaticViewSitemap
 
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("", include("portal.urls")),
+    path(
+    "sitemap.xml",
+    sitemap,
+    {"sitemaps": sitemaps},
+    name="django.contrib.sitemaps.views.sitemap",
+   ),
 ]
