b056eaedf7a8068ba081e5b5a236e7fd02cf57fe
KizTech <kiztech01@gmail.com>
2026-09-20 16:40:11 +0100

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, reverse_lazy
+from django.contrib.sitemaps.views import sitemap
+from portal.sitemaps import StaticViewSitemap
 
 from . import views
 
