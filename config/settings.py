99c5bd9c88a81e70622f0ef68dfbc52bbd0294f5
KizTech <kiztech01@gmail.com>
2026-09-20 16:33:53 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sitemaps",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
